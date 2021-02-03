ca7b211e77fcb13ef02ba1b87be0fb5fa576f2bd
Woloszyn97 <73226245+Woloszyn97@users.noreply.github.com>
2021-02-03 11:10:52 +0100

    Dodanie

--- Mysin.h
+++ Mysin.h
@@ -4,28 +4,34 @@
 
 
   
-
+/**Utworzona klasa MySin*/
 class MySin
 {
+
     public:
-	
+/**Metoda publiczna MySin()*/
+
          MySin();
-		
+		 
+/**Metoda publiczna ~Mysin()*/
          ~MySin();
-		
+			
+/** Metoda publiczna MySin(double x )*/
          MySin(double x);
-		
+			/** Metoda publiczna MySin(const MySin &obj)*/
          MySin(const MySin &obj);
-		
+			/** Metoda publiczna double value()*/
         double value();
-		
+			/** Metoda publiczna void setx(double)*/
         void setX(double);
-		
+			/** Metoda publiczna double getX()*/
         double getX();
 	
 
         private:
+		/** Składowa prywatna double mX() */
 	
+	/**Funkcja value ma zwracać wartość \f$\sin(x)\f$ , która jest wyliczana na podstawie pierwszych 10 wyrazów szeregu \f$\sin(x)=\sum_{k=0}^\infty(-1)^k\frac{x^{2k+1}}{(2k+1)!}\f$ \image html wykres.png*/
             double mX();
 };
 
